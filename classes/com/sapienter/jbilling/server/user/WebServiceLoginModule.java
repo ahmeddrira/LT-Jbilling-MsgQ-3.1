154ba5415acde810568759e593f583fb0d57a7af
Emiliano Conde <goldrimtang@users.sourceforge.net>
2007-03-08 06:41:27 +0000

    Encryption of credit card data and user passwords (new files)

---
+++ classes/com/sapienter/jbilling/server/user/WebServiceLoginModule.java
@@ -0,0 +1,25 @@
+package com.sapienter.jbilling.server.user;
+
+import java.security.acl.Group;
+
+import javax.security.auth.login.LoginException;
+
+import org.apache.log4j.Logger;
+import org.jboss.security.auth.spi.DatabaseServerLoginModule;
+
+import com.sapienter.jbilling.common.JBCrypto;
+
+public class WebServiceLoginModule extends DatabaseServerLoginModule {
+	private static Logger LOG = Logger.getLogger(WebServiceLoginModule.class);
+
+	@Override
+	protected boolean validatePassword(String inputPassword,
+			String expectedPassword) {
+
+		inputPassword = JBCrypto.getPasswordCrypto(null).encrypt(inputPassword);		
+
+        LOG.debug("ws: validating " + inputPassword + "=" + expectedPassword);
+		return super.validatePassword(inputPassword, expectedPassword);
+	}
+
+}

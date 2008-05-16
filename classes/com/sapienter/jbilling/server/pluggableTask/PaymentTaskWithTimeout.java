caea86e8126a0c4c075a85e489684e157a4febdf
Emiliano Conde <goldrimtang@users.sourceforge.net>
2008-05-16 16:35:17 +0000

    Added a default value

--- classes/com/sapienter/jbilling/server/pluggableTask/PaymentTaskWithTimeout.java
+++ classes/com/sapienter/jbilling/server/pluggableTask/PaymentTaskWithTimeout.java
@@ -1,3 +1,22 @@
+/*
+    jbilling - The Enterprise Open Source Billing System
+    Copyright (C) 2003-2007 Sapienter Billing Software Corp. and Emiliano Conde
+
+    This file is part of jbilling.
+
+    jbilling is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    jbilling is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with jbilling.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package com.sapienter.jbilling.server.pluggableTask;
 
 import com.sapienter.jbilling.server.pluggableTask.admin.PluggableTaskDTO;
@@ -14,7 +33,7 @@ public abstract class PaymentTaskWithTimeout extends PaymentTaskBase {
 		
 		super.initializeParamters(task);
 
-		String timeoutText = ensureGetParameter(PARAM_TIMEOUT_SECONDS);
+		String timeoutText = getOptionalParameter(PARAM_TIMEOUT_SECONDS, "10");
 		try {
 			myTimeout = Integer.parseInt(timeoutText);  
 		} catch (NumberFormatException e){

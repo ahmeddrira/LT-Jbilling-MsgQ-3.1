76e41f14c27ced7bb7feb46b0cae0a64a1cc5aa7
Emiliano Conde <goldrimtang@users.sourceforge.net>
2008-10-03 16:32:29 +0000

    Do not cache connections, to allow multiple simultaneous calls.

--- classes/com/sapienter/jbilling/server/util/api/JbillingAPIFactory.java
+++ classes/com/sapienter/jbilling/server/util/api/JbillingAPIFactory.java
@@ -23,49 +23,49 @@ import java.io.IOException;
 import java.util.Properties;
 
 public final class JbillingAPIFactory {
-    static private JbillingAPI api = null;
     static private Properties config = null;
     
     private JbillingAPIFactory() {}; // a factory should not be instantiated
     
     static public JbillingAPI getAPI() 
             throws JbillingAPIException, IOException {
-    	if (api == null) {
+    	if (config == null) {
             String propFile = System.getProperty("JBILLING_PROPERTIES_FILE", "/jbilling_api.properties");
             config = new Properties();
             config.load(JbillingAPI.class.getResourceAsStream(propFile));
+        }
 
-            String default_api = config.getProperty("default", "axis");
-            // check for AXIS
-            if (default_api.compareToIgnoreCase("axis") == 0) {
-                String userName = config.getProperty("user_name");
-                String password = config.getProperty("password");
-                String endPoint = config.getProperty("end_point");
-                if (userName == null || password == null || endPoint == null) {
-                    throw new JbillingAPIException("properties user_name " +
-                            "password end_point are required for AXIS");
-                }
-                String timeOutStr = config.getProperty("timeout");
-                int timeOut = 30;
-                if (timeOutStr != null && timeOutStr.length() > 0) {
-                    timeOut = Integer.valueOf(timeOutStr);
-                }
-                timeOut *= 1000; // convert to mills
-                api =  new AxisAPI(userName,password,endPoint,timeOut );
-            } else if (default_api.compareToIgnoreCase("ejb") == 0) {
-                String userName = config.getProperty("user_name");
-                String password = config.getProperty("password");
-                String provider_url = config.getProperty("provider_url");
-                if (userName == null || password == null || provider_url == null) {
-                    throw new JbillingAPIException("properties user_name [" + userName +
-                            "] password [" + password + 
-                            "] provider_url [" + provider_url + 
-                            "] are required for EJB");
-                }
-                api =  new EJBAPI(provider_url, userName, password);
-            } else {
-                throw new JbillingAPIException("api [" + default_api + "] is not supported");
+        JbillingAPI api = null;
+        String default_api = config.getProperty("default", "axis");
+        // check for AXIS
+        if (default_api.compareToIgnoreCase("axis") == 0) {
+            String userName = config.getProperty("user_name");
+            String password = config.getProperty("password");
+            String endPoint = config.getProperty("end_point");
+            if (userName == null || password == null || endPoint == null) {
+                throw new JbillingAPIException("properties user_name " +
+                        "password end_point are required for AXIS");
             }
+            String timeOutStr = config.getProperty("timeout");
+            int timeOut = 30;
+            if (timeOutStr != null && timeOutStr.length() > 0) {
+                timeOut = Integer.valueOf(timeOutStr);
+            }
+            timeOut *= 1000; // convert to mills
+            api =  new AxisAPI(userName,password,endPoint,timeOut );
+        } else if (default_api.compareToIgnoreCase("ejb") == 0) {
+            String userName = config.getProperty("user_name");
+            String password = config.getProperty("password");
+            String provider_url = config.getProperty("provider_url");
+            if (userName == null || password == null || provider_url == null) {
+                throw new JbillingAPIException("properties user_name [" + userName +
+                        "] password [" + password + 
+                        "] provider_url [" + provider_url + 
+                        "] are required for EJB");
+            }
+            api =  new EJBAPI(provider_url, userName, password);
+        } else {
+            throw new JbillingAPIException("api [" + default_api + "] is not supported");
         }
     	return api;
     }

a852c6859916735293f0d188b6ac2d702bf93ae9
Emiliano Conde <goldrimtang@users.sourceforge.net>
2008-12-04 23:37:21 +0000

    Removed tabs (formatting), and moved the mediation process above the billing process

--- classes/com/sapienter/jbilling/client/process/Trigger.java
+++ classes/com/sapienter/jbilling/client/process/Trigger.java
@@ -1,22 +1,18 @@
 /*
-    jBilling - The Enterprise Open Source Billing System
-    Copyright (C) 2003-2008 Enterprise jBilling Software Ltd. and Emiliano Conde
-
-    This file is part of jbilling.
-
-    jbilling is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    jbilling is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with jbilling.  If not, see <http://www.gnu.org/licenses/>.
-*/
+jBilling - The Enterprise Open Source Billing System
+Copyright (C) 2003-2008 Enterprise jBilling Software Ltd. and Emiliano Conde
+This file is part of jbilling.
+jbilling is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+jbilling is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with jbilling.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /*
  * Created on Jul 14, 2004
@@ -64,10 +60,10 @@ import com.sapienter.jbilling.server.mediation.MediationSessionHome;
  * @author Emil
  *
  */
-public class Trigger implements Job{
-	
+public class Trigger implements Job {
+
     private static final Logger LOG = Logger.getLogger(Trigger.class);
-    
+
     /**
      * Initialize tool Trigger. Load properties from jbilling.properties and set up Quartz job/trigger
      * 
@@ -84,233 +80,219 @@ public class Trigger implements Job{
      *
      */
     public static void Initialize() {
-    	// Load properties from jbilling.properties
-    	String time = null;
-    	String frequency = null;
-    	
-    	try {
-    		time = Util.getSysProp("process.time");
-    		frequency = Util.getSysProp("process.frequency");
-    	} catch (Exception e){
-    		// just eat it
-    	}
-    	
-    	// both null or empty, log one message and return
-    	if( (time == null || time.length() == 0) && (frequency==null || frequency.length() == 0) )
-    	{
-    		LOG.info("No schedule information found.");
-    		return;
-    	}
-    	
-    	Date startTime = null;
-    	int interval = 0;
-    	
-    	// if process.time absents and frequency is zero, fire every 0:00AM
-    	GregorianCalendar cal = new GregorianCalendar();
-    	if( ( time == null || time.length() == 0 ) && "0".equals(frequency)) {
-    		cal.add(Calendar.DATE, 1);
-    		startTime = Util.truncateDate(cal.getTime());
-    		interval = 24 * 60;
-    	} else if(time == null || time.length() == 0) { // process.time is not present, start at startup + frequency
-    		try {
-    			interval = Integer.parseInt(frequency);
-    		} catch(NumberFormatException e) {
-    			LOG.debug(e);
-				LOG.info("Error:" + e.getMessage() + " Schedule does not start.");
-				
-				// Leave
-				return;
-    		}
-    		cal.add(Calendar.MINUTE, interval);
-    		startTime = Util.truncateDate(cal.getTime());
-    	} else { // Its normal one
-    		SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd-HHmm");
-    		try {
-    			interval = Integer.parseInt(frequency);
-    			if(interval == 0) {
-    				LOG.info("The frequency can not be zero when time is specified.");
-    				return;
-    			}
-				startTime = df.parse(time);
-			} catch (ParseException e) {
-				LOG.debug(e);
-				LOG.info("Error:" + e.getMessage() + " Schedule does not start.");
-				// Leave
-				return;
-			} catch(NumberFormatException e) {
-				LOG.debug(e);
-				LOG.info("Error:" + e.getMessage() + " Schedule does not start.");
-				//Leave
-				return;
-			}			
-    	}
-    	
-    	// startup trigger
-    	try {
-    		SchedulerFactory schedFact = new org.quartz.impl.StdSchedulerFactory();
-			Scheduler sched = schedFact.getScheduler();
-			JobDetail jbillingJob = new JobDetail("jbilling", Scheduler.DEFAULT_GROUP, Trigger.class);
-			
-			SimpleTrigger trigger = new SimpleTrigger("jbillingTrigger", 
-					Scheduler.DEFAULT_GROUP, 
-					startTime,
-					null,
-					SimpleTrigger.REPEAT_INDEFINITELY,
-					interval * 60 * 1000);
-			
-			trigger.setMisfireInstruction(SimpleTrigger.MISFIRE_INSTRUCTION_RESCHEDULE_NEXT_WITH_EXISTING_COUNT);
-			
-			sched.scheduleJob(jbillingJob, trigger);
-			
-			sched.start();
-		} catch (SchedulerException e) {
-			LOG.debug(e);
-		}
-    }
-    
-	public static void main(String[] args) {
- 
-	}
+        // Load properties from jbilling.properties
+        String time = null;
+        String frequency = null;
 
-	public void execute(JobExecutionContext ctx) throws JobExecutionException {
-		BillingProcessSession remoteBillingProcess = null;
-		
-		try {
-			// get a session for the remote interfaces
-			BillingProcessSessionHome billingProcessHome =
-				(BillingProcessSessionHome) JNDILookup.getFactory(true).lookUpHome(
-						BillingProcessSessionHome.class,
-						BillingProcessSessionHome.JNDI_NAME);
-			remoteBillingProcess = billingProcessHome.create();
-			UserSessionHome userHome =
-				(UserSessionHome) JNDILookup.getFactory(true).lookUpHome(
-						UserSessionHome.class,
-						UserSessionHome.JNDI_NAME);
-			UserSession remoteUser = userHome.create();
-			OrderSessionHome orderHome =
-				(OrderSessionHome) JNDILookup.getFactory(true).lookUpHome(
-						OrderSessionHome.class,
-						OrderSessionHome.JNDI_NAME);
-			OrderSession remoteOrder = orderHome.create();
-			InvoiceSessionHome invoiceHome =
-				(InvoiceSessionHome) JNDILookup.getFactory(true).lookUpHome(
-						InvoiceSessionHome.class,
-						InvoiceSessionHome.JNDI_NAME);
-			InvoiceSession remoteInvoice = invoiceHome.create();
-			ListSessionHome listHome =
-				(ListSessionHome) JNDILookup.getFactory(true).lookUpHome(
-						ListSessionHome.class,
-						ListSessionHome.JNDI_NAME);
-			ListSession remoteList = listHome.create();
+        try {
+            time = Util.getSysProp("process.time");
+            frequency = Util.getSysProp("process.frequency");
+        } catch (Exception e) {
+        // just eat it
+        }
+
+        // both null or empty, log one message and return
+        if ((time == null || time.length() == 0) && (frequency == null || frequency.length() == 0)) {
+            LOG.info("No schedule information found.");
+            return;
+        }
+
+        Date startTime = null;
+        int interval = 0;
+
+        // if process.time absents and frequency is zero, fire every 0:00AM
+        GregorianCalendar cal = new GregorianCalendar();
+        if ((time == null || time.length() == 0) && "0".equals(frequency)) {
+            cal.add(Calendar.DATE, 1);
+            startTime = Util.truncateDate(cal.getTime());
+            interval = 24 * 60;
+        } else if (time == null || time.length() == 0) { // process.time is not present, start at startup + frequency
+            try {
+                interval = Integer.parseInt(frequency);
+            } catch (NumberFormatException e) {
+                LOG.debug(e);
+                LOG.info("Error:" + e.getMessage() + " Schedule does not start.");
+
+                // Leave
+                return;
+            }
+            cal.add(Calendar.MINUTE, interval);
+            startTime = Util.truncateDate(cal.getTime());
+        } else { // Its normal one
+            SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd-HHmm");
+            try {
+                interval = Integer.parseInt(frequency);
+                if (interval == 0) {
+                    LOG.info("The frequency can not be zero when time is specified.");
+                    return;
+                }
+                startTime = df.parse(time);
+            } catch (ParseException e) {
+                LOG.debug(e);
+                LOG.info("Error:" + e.getMessage() + " Schedule does not start.");
+                // Leave
+                return;
+            } catch (NumberFormatException e) {
+                LOG.debug(e);
+                LOG.info("Error:" + e.getMessage() + " Schedule does not start.");
+                //Leave
+                return;
+            }
+        }
+
+        // startup trigger
+        try {
+            SchedulerFactory schedFact = new org.quartz.impl.StdSchedulerFactory();
+            Scheduler sched = schedFact.getScheduler();
+            JobDetail jbillingJob = new JobDetail("jbilling", Scheduler.DEFAULT_GROUP, Trigger.class);
+
+            SimpleTrigger trigger = new SimpleTrigger("jbillingTrigger",
+                    Scheduler.DEFAULT_GROUP,
+                    startTime,
+                    null,
+                    SimpleTrigger.REPEAT_INDEFINITELY,
+                    interval * 60 * 1000);
+
+            trigger.setMisfireInstruction(SimpleTrigger.MISFIRE_INSTRUCTION_RESCHEDULE_NEXT_WITH_EXISTING_COUNT);
+
+            sched.scheduleJob(jbillingJob, trigger);
+
+            sched.start();
+        } catch (SchedulerException e) {
+            LOG.debug(e);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public void execute(JobExecutionContext ctx) throws JobExecutionException {
+        BillingProcessSession remoteBillingProcess = null;
+
+        try {
+            // get a session for the remote interfaces
+            BillingProcessSessionHome billingProcessHome =
+                    (BillingProcessSessionHome) JNDILookup.getFactory(true).lookUpHome(
+                    BillingProcessSessionHome.class,
+                    BillingProcessSessionHome.JNDI_NAME);
+            remoteBillingProcess = billingProcessHome.create();
+            UserSessionHome userHome =
+                    (UserSessionHome) JNDILookup.getFactory(true).lookUpHome(
+                    UserSessionHome.class,
+                    UserSessionHome.JNDI_NAME);
+            UserSession remoteUser = userHome.create();
+            OrderSessionHome orderHome =
+                    (OrderSessionHome) JNDILookup.getFactory(true).lookUpHome(
+                    OrderSessionHome.class,
+                    OrderSessionHome.JNDI_NAME);
+            OrderSession remoteOrder = orderHome.create();
+            InvoiceSessionHome invoiceHome =
+                    (InvoiceSessionHome) JNDILookup.getFactory(true).lookUpHome(
+                    InvoiceSessionHome.class,
+                    InvoiceSessionHome.JNDI_NAME);
+            InvoiceSession remoteInvoice = invoiceHome.create();
+            ListSessionHome listHome =
+                    (ListSessionHome) JNDILookup.getFactory(true).lookUpHome(
+                    ListSessionHome.class,
+                    ListSessionHome.JNDI_NAME);
+            ListSession remoteList = listHome.create();
             MediationSessionHome mediationHome =
-                (MediationSessionHome) JNDILookup.getFactory(true).lookUpHome(
-                        MediationSessionHome.class,
-                        MediationSessionHome.JNDI_NAME);
+                    (MediationSessionHome) JNDILookup.getFactory(true).lookUpHome(
+                    MediationSessionHome.class,
+                    MediationSessionHome.JNDI_NAME);
             MediationSession remoteMediation = mediationHome.create();
 
-			// determine the date for this run
-			Date today = Calendar.getInstance().getTime();
-			today = Util.truncateDate(today);
+            // determine the date for this run
+            Date today = Calendar.getInstance().getTime();
+            today = Util.truncateDate(today);
 
-			// Determine if this call first time of today
-			boolean firstOfToday = true;
-			Date lastFire = ctx.getPreviousFireTime();
-			
-			if(lastFire == null) { // Should be first time call
-				firstOfToday = true;
-			} else {
-				lastFire = Util.truncateDate(lastFire);
-				firstOfToday = lastFire.before(today);
-			}
+            // Determine if this call first time of today
+            boolean firstOfToday = true;
+            Date lastFire = ctx.getPreviousFireTime();
 
-			// run the billing process
-            if (Util.getSysPropBooleanTrue("process.run_billing")) {
-    			LOG.info("Running trigger for " + today);
-    			LOG.info("Starting billing process at " + 
-    					Calendar.getInstance().getTime());
-    			remoteBillingProcess.trigger(today);
-    			LOG.info("Ended billing process at " + 
-    					Calendar.getInstance().getTime());
+            if (lastFire == null) { // Should be first time call
+                firstOfToday = true;
+            } else {
+                lastFire = Util.truncateDate(lastFire);
+                firstOfToday = lastFire.before(today);
+            }
+            
+            if (Util.getSysPropBooleanTrue("process.run_mediation")) {
+                LOG.info("Starting mediation at " + Calendar.getInstance().getTime());
+                remoteMediation.trigger();
+                LOG.info("Ended mediation at " + Calendar.getInstance().getTime());
             }
 
-			// now the ageing process
-			if (firstOfToday) {
+
+            // run the billing process
+            if (Util.getSysPropBooleanTrue("process.run_billing")) {
+                LOG.info("Running trigger for " + today);
+                LOG.info("Starting billing process at " +
+                        Calendar.getInstance().getTime());
+                remoteBillingProcess.trigger(today);
+                LOG.info("Ended billing process at " +
+                        Calendar.getInstance().getTime());
+            }
+
+            // now the ageing process
+            if (firstOfToday) {
                 if (Util.getSysPropBooleanTrue("process.run_ageing")) {
-    				LOG.info("Starting ageing process at " + 
-    						Calendar.getInstance().getTime());
-    				remoteBillingProcess.reviewUsersStatus(today);
-    				LOG.info("Ended ageing process at " + 
-    						Calendar.getInstance().getTime());
+                    LOG.info("Starting ageing process at " +
+                            Calendar.getInstance().getTime());
+                    remoteBillingProcess.reviewUsersStatus(today);
+                    LOG.info("Ended ageing process at " +
+                            Calendar.getInstance().getTime());
                 }
 
                 if (Util.getSysPropBooleanTrue("process.run_partner")) {
                     // now the partner payout process
-                    LOG.info("Starting partner process at "
-                            + Calendar.getInstance().getTime());
+                    LOG.info("Starting partner process at " + Calendar.getInstance().getTime());
                     remoteUser.processPayouts(today);
-                    LOG.info("Ended partner process at "
-                            + Calendar.getInstance().getTime());
-                }		
-                
+                    LOG.info("Ended partner process at " + Calendar.getInstance().getTime());
+                }
+
                 if (Util.getSysPropBooleanTrue("process.run_order_expire")) {
                     // finally the orders about to expire notification
-                    LOG.info("Starting order notification at "
-                            + Calendar.getInstance().getTime());
+                    LOG.info("Starting order notification at " + Calendar.getInstance().getTime());
                     remoteOrder.reviewNotifications(today);
-                    LOG.info("Ended order notification at "
-                            + Calendar.getInstance().getTime());
-                }                
+                    LOG.info("Ended order notification at " + Calendar.getInstance().getTime());
+                }
                 if (Util.getSysPropBooleanTrue("process.run_invoice_reminder")) {
                     // the invoice reminders
-                    LOG.info("Starting invoice reminders at "
-                            + Calendar.getInstance().getTime());
+                    LOG.info("Starting invoice reminders at " + Calendar.getInstance().getTime());
                     remoteInvoice.sendReminders(today);
-                    LOG.info("Ended invoice reminders at "
-                            + Calendar.getInstance().getTime());
-                }                
+                    LOG.info("Ended invoice reminders at " + Calendar.getInstance().getTime());
+                }
                 if (Util.getSysPropBooleanTrue("process.run_penalty")) {
                     // the invoice penalties
-                    LOG.info("Starting invoice penalties at "
-                            + Calendar.getInstance().getTime());
+                    LOG.info("Starting invoice penalties at " + Calendar.getInstance().getTime());
                     remoteInvoice.processOverdue(today);
-                    LOG.info("Ended invoice penalties at "
-                            + Calendar.getInstance().getTime());
-                }                
+                    LOG.info("Ended invoice penalties at " + Calendar.getInstance().getTime());
+                }
                 if (Util.getSysPropBooleanTrue("process.run_list")) {
                     // update the listing statistics
-                    LOG.info("Starting list stats at "
-                            + Calendar.getInstance().getTime());
+                    LOG.info("Starting list stats at " + Calendar.getInstance().getTime());
                     remoteList.updateStatistics();
-                    LOG.info("Ended list stats at "
-                            + Calendar.getInstance().getTime());
-                }                
+                    LOG.info("Ended list stats at " + Calendar.getInstance().getTime());
+                }
                 if (Util.getSysPropBooleanTrue("process.run_cc_expire")) {
                     // send credit card expiration emails
-                    LOG.info("Starting credit card expiration at "
-                            + Calendar.getInstance().getTime());
+                    LOG.info("Starting credit card expiration at " + Calendar.getInstance().getTime());
                     remoteUser.notifyCreditCardExpiration(today);
-                    LOG.info("Ended credit card expiration at "
-                            + Calendar.getInstance().getTime());
-                }                
-                if (Util.getSysPropBooleanTrue("process.run_mediation")) {
-                    // send credit card expiration emails
-                    LOG.info("Starting mediation at "
-                            + Calendar.getInstance().getTime());
-                    remoteMediation.trigger();
-                    LOG.info("Ended mediation at "
-                            + Calendar.getInstance().getTime());
-                }                
-			}
+                    LOG.info("Ended credit card expiration at " + Calendar.getInstance().getTime());
+                }
+            }
 
-		} catch (RemoteException e) {
-			LOG.debug(e);
-		} catch (NamingException e) {
-			LOG.debug(e);
-		} catch (CreateException e) {
-			LOG.debug(e);
-		} catch (SessionInternalError e) {
-			LOG.debug(e);
-		}
-		
-	}
+        } catch (RemoteException e) {
+            LOG.debug(e);
+        } catch (NamingException e) {
+            LOG.debug(e);
+        } catch (CreateException e) {
+            LOG.debug(e);
+        } catch (SessionInternalError e) {
+            LOG.debug(e);
+        }
+
+    }
 }

7e7f7788e6dc7274dc57df9ce3b8a88c16aa0de3
Emiliano Conde <goldrimtang@users.sourceforge.net>
2009-05-19 15:48:53 +0000

    Change from GPL to AGPL

--- classes/com/sapienter/jbilling/server/system/event/task/IInternalEventsTask.java
+++ classes/com/sapienter/jbilling/server/system/event/task/IInternalEventsTask.java
@@ -1,20 +1,20 @@
 /*
     jBilling - The Enterprise Open Source Billing System
-    Copyright (C) 2003-2008 Enterprise jBilling Software Ltd. and Emiliano Conde
+    Copyright (C) 2003-2009 Enterprise jBilling Software Ltd. and Emiliano Conde
 
     This file is part of jbilling.
 
     jbilling is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
+    it under the terms of the GNU Affero General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
     jbilling is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+    GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
+    You should have received a copy of the GNU Affero General Public License
     along with jbilling.  If not, see <http://www.gnu.org/licenses/>.
 */
 package com.sapienter.jbilling.server.system.event.task;

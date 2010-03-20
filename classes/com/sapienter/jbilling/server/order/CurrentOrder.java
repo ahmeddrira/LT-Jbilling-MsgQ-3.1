9a0b59d3e39ace51d736483dfbd2437a31e67d4d
Emiliano Conde <goldrimtang@users.sourceforge.net>
2010-03-19 20:36:32 -0400

    Fixed bug where there would be a NPE when a one-time order for the needed date was present with a 'finished' status.

--- classes/com/sapienter/jbilling/server/order/CurrentOrder.java
+++ classes/com/sapienter/jbilling/server/order/CurrentOrder.java
@@ -97,6 +97,7 @@ public class CurrentOrder {
         if (subscriptionId == null) {
             return null;
         }
+        Integer mainOrder;
         try {
             order = new OrderBL(subscriptionId);
             entityId = order.getEntity().getBaseUserByUserId().getCompany().getId();
@@ -108,8 +109,10 @@ public class CurrentOrder {
         
         int futurePeriods = 0;
         boolean orderFound = false;
+        mainOrder = order.getEntity().getId();
         do {
             LOG.debug("Calculating one timer date. Future periods " + futurePeriods);
+            order.set(mainOrder);
             final Date newOrderDate = calculateDate(futurePeriods);
             if (newOrderDate == null) {
                 // this is an error, there isn't a good date give the event date and

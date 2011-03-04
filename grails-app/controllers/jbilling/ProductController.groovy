aa232a2860741b4f58c1a1053243746e93e066b0
Brian Cowdery <brianc@jbilling.com>
2011-03-04 15:45:09 -0700

    Download product CSV for "show all" products view

--- grails-app/controllers/jbilling/ProductController.groovy
+++ grails-app/controllers/jbilling/ProductController.groovy
@@ -11,9 +11,7 @@ import com.sapienter.jbilling.server.user.db.CompanyDTO
 import grails.plugins.springsecurity.Secured
 import com.sapienter.jbilling.server.pricing.PriceModelWS
 import org.codehaus.groovy.grails.web.servlet.mvc.GrailsParameterMap
-import com.sapienter.jbilling.server.pricing.util.AttributeUtils
-import com.sapienter.jbilling.server.pricing.db.PriceModelStrategy
-import com.sapienter.jbilling.server.pricing.strategy.PricingStrategy
+
 import com.sapienter.jbilling.client.pricing.util.PlanHelper
 import com.sapienter.jbilling.server.util.csv.CsvExporter
 import com.sapienter.jbilling.server.util.csv.Exporter
@@ -41,7 +39,7 @@ class ProductController {
     def list = {
         def filters = filterService.getFilters(FilterType.PRODUCT, params)
         def categories = getCategories()
-        def products = params.id ? getItemsByTypeId(params.int('id'), filters) : null
+        def products = params.id ? getProducts(params.int('id'), filters) : null
         def categoryId = params.int('id') ?: products ? products.get(0)?.itemTypes?.asList()?.get(0)?.id : null
 
         breadcrumbService.addBreadcrumb(controllerName, actionName, null, params.int('id'))
@@ -75,7 +73,7 @@ class ProductController {
     def products = {
         if (params.id) {
             def filters = filterService.getFilters(FilterType.PRODUCT, params)
-            def products = getItemsByTypeId(params.int('id'), filters)
+            def products = getProducts(params.int('id'), filters)
 
             breadcrumbService.addBreadcrumb(controllerName, 'list', null, params.int('id'))
 
@@ -90,7 +88,7 @@ class ProductController {
         def filters = filterService.getFilters(FilterType.PRODUCT, params)
 
         params.max = CsvExporter.MAX_RESULTS
-        def products = getItemsByTypeId(params.int('id'), filters)
+        def products = getProducts(params.int('id'), filters)
 
         if (products.totalCount > CsvExporter.MAX_RESULTS) {
             flash.error = message(code: 'error.export.exceeds.maximum')
@@ -103,7 +101,7 @@ class ProductController {
         }
     }
 
-    def getItemsByTypeId(Integer id, filters) {
+    def getProducts(Integer id, filters) {
         params.max = params?.max?.toInteger() ?: pagination.max
         params.offset = params?.offset?.toInteger() ?: pagination.offset
 
@@ -118,9 +116,13 @@ class ProductController {
 
                     }
                 }
-                itemTypes {
-                    eq('id', id)
+
+                if (id != null) {
+                    itemTypes {
+                        eq('id', id)
+                    }
                 }
+
                 isEmpty('plans')
                 eq('deleted', 0)
                 eq('entity', new CompanyDTO(session['company_id']))
@@ -135,33 +137,7 @@ class ProductController {
     def allProducts = {
         def filters = filterService.getFilters(FilterType.PRODUCT, params)
 
-        params.max = params?.max?.toInteger() ?: pagination.max
-        params.offset = params?.offset?.toInteger() ?: pagination.offset
-
-        def products = ItemDTO.createCriteria().list(
-                max:    params.max,
-                offset: params.offset
-        ) {
-            and {
-                filters.each { filter ->
-                    if (filter.value) {
-                        switch (filter.constraintType) {
-                            case FilterConstraint.EQ:
-                                eq(filter.field, filter.value)
-                                break
-
-                            case FilterConstraint.LIKE:
-                                like(filter.field, filter.stringValue)
-                                break
-                        }
-                    }
-                }
-
-                isEmpty('plans')
-                eq('deleted', 0)
-                eq('entity', new CompanyDTO(session['company_id']))
-            }
-        }
+        def products =  getProducts(null, filters)
 
         render template: 'products', model: [ products: products ]
     }
@@ -186,7 +162,7 @@ class ProductController {
             def categories = getCategories();
 
             def productCategoryId = params.category ?: product?.itemTypes?.asList()?.get(0)?.id
-            def products = getItemsByTypeId(productCategoryId, filters);
+            def products = getProducts(productCategoryId, filters);
 
             render view: 'list', model: [ categories: categories, products: products, selectedProduct: product, selectedCategoryId: productCategoryId, filters: filters ]
         }

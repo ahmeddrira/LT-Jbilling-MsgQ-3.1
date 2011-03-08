a72e5780c0bdc52378d89a1eb10952205ddce1a3
Emiliano Conde <goldrimtang@users.sourceforge.net>
2011-03-08 14:35:02 -0500

    Removed a garbage line from upgarde.sql

--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 -- this script will upgrade a database schema from the latest jbilling release
 -- to the code currently at the tip of the trunk.
 -- It is tested on postgreSQL, but it is meant to be ANSI SQL

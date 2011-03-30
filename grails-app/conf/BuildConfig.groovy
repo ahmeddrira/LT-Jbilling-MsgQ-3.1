064e560cdd31ca2f7b67022cb2bcb07d2222e8e7
Brian Cowdery <brianc@jbilling.com>
2011-03-30 15:44:49 -0600

    Support for external config files in env JBILLING_HOME
    * jbilling-Config.groovy and jbilling-DataSource.groovy

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,14 +1,17 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.war.file = "target/${appName}.war"
+
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
         // uncomment to disable ehcache
         // excludes 'ehcache'
     }
+
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+
     repositories {
         grailsPlugins()
         grailsHome()
@@ -23,6 +26,7 @@ grails.project.dependency.resolution = {
         mavenRepo "http://download.java.net/maven/2/"
         mavenRepo "http://repository.jboss.org/nexus/content/groups/public-jboss/"
     }
+
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -12,6 +12,15 @@ import org.apache.log4j.*
 //    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
 // }
 
+def home = System.getenv("JBILLING_HOME") ?: System.getProperty("JBILLING_HOME")
+if (home) {
+    grails.config.locations = [
+            "file:${home}/${appName}.properties",
+            "file:${home}/${appName}-Config.groovy",
+            "file:${home}/${appName}-DataSource.groovy",
+    ]
+}
+
 grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
 grails.mime.use.accept.header = false
@@ -28,10 +37,12 @@ grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
                       form: 'application/x-www-form-urlencoded',
                       multipartForm: 'multipart/form-data'
                     ]
+
 // The default codec used to encode data with ${}
 grails.views.default.codec = "none" // none, html, base64
 grails.views.gsp.encoding = "UTF-8"
 grails.converters.encoding = "UTF-8"
+
 // enable Sitemesh preprocessing of GSP pages
 grails.views.gsp.sitemesh.preprocess = true
 // scaffolding templates configuration
@@ -60,46 +71,6 @@ environments {
     }
 
 }
-
-//log4j = {
-//    
-//    appenders {
-//        console name:"CONSOLE", layout:pattern(conversionPattern: "%d{ABSOLUTE} %-5p [%c{1}] %m%n")
-//        rollingFile name:"serverAppender", datepattern: "'.'yyyy-MM-dd", file: "logs/server.log", layout:pattern(conversionPattern: "%d %-5r %-5p [%c] (%t:%x) %m%n")
-//        rollingFile name:"jbillingAppender", datepattern: "'.'yyyy-MM-dd", file: "logs/jbilling.log", layout:pattern(conversionPattern: "%d %-5p [%c] %m%n"), append:false
-//        rollingFile name:"hibernateAppender", datepattern: "'.'yyyy-MM-dd", file: "logs/sql.log", layout:pattern(conversionPattern: "%d %-5r %-5p [%c] (%t:%x) %m%n"), append:false
-//    }
-//    
-//    debug jbillingAppender:'com.sapienter.jbilling'
-//	info jbillingAppender:'grails.app'
-//	debug jbillingAppender:'grails.app.controller'
-//	debug jbillingAppender:'grails.app.service'
-//	debug jbillingAppender:'grails.app.tagLib'
-//    info jbillingAppender:'com.sapienter.jbilling.client.authentication.CompanyUserRememberMeFilter'
-//    
-//    /*
-//     * Hibernate logging:
-//     * org.hibernate.SQL           Log all SQL DML statements as they are executed
-//     * org.hibernate.type          Log all JDBC parameters
-//     * org.hibernate.tool.hbm2ddl  Log all SQL DDL statements as they are executed
-//     * org.hibernate.pretty        Log the state of all entities (max 20 entities) associated with the session at flush time
-//     * org.hibernate.cache         Log all second-level cache activity
-//     * org.hibernate.transaction   Log transaction related activity
-//     * org.hibernate.jdbc          Log all JDBC resource acquisition
-//     * org.hibernate.hql.ast.AST   Log HQL and SQL ASTs during query parsing
-//     * org.hibernate.secure        Log all JAAS authorization requests
-//     * org.hibernate               Log everything. This is a lot of information but it is useful for troubleshooting
-//     */
-//    
-//    // use the hibernateAppender. If you use the jBillingAppender, some entries are not logged :( (bug?)
-//    //debug hibernateAppender:'org.hibernate.SQL'
-//    //debug hibernateAppender:'org.hibernate.SQL.type'
-//    
-//    root {
-//        info 'CONSOLE','serverAppender'
-//        additivity = true
-//    }
-//}
 
 /*
     Spring Security

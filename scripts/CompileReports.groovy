602b91a4306873fed093bf0444871e05082fdf90
Brian Cowdery <brianc@jbilling.com>
2011-04-14 21:27:05 -0600

    Copy resources and compile invoice design scripts

--- scripts/CompileReports.groovy
+++ scripts/CompileReports.groovy
@@ -1,22 +1,25 @@
 includeTargets << grailsScript("Init")
 
+final tempDir = "${basedir}/tmp"
+final resourcesDir = "${basedir}/resources"
+final descriptorsDir = "${basedir}/descriptors"
+
 target(main: "Compiles jasper report designs.") {
+    ant.taskdef(name: "jrc", classname: "net.sf.jasperreports.ant.JRAntCompileTask" )
 
-    ant.taskdef( name: "jrc", classname: "net.sf.jasperreports.ant.JRAntCompileTask" )
+    delete(dir: "${resourcesDir}/reports")
+    mkdir(dir: "${resourcesDir}/reports")
 
-    delete( dir: "./resources/reports" )
-    mkdir( dir: "./resources/reports" )
-
-    mkdir( dir: "./tmp" )
-    jrc( destdir: "./resources/reports", tempdir: "./tmp", keepjava: "true", xmlvalidation: "true" ) {
+    mkdir(dir: tempDir)
+    jrc(destdir: "${resourcesDir}/reports", tempdir: tempDir, keepjava: "true", xmlvalidation: "true") {
         src {
-            fileset( dir: "./descriptors/reports", includes: "**/*.jrxml" )
+            fileset(dir: "${descriptorsDir}/reports", includes: "**/*.jrxml")
         }
         classpath {
-            path( refid: compileClasspath )
+            path(refid: compileClasspath)
         }
     }
-    delete( dir: "./tmp")
+    delete(dir: tempDir)
 }
 
 setDefaultTarget(main)
---
+++ scripts/CopyResources.groovy
@@ -0,0 +1,37 @@
+includeTargets << grailsScript("Init")
+
+final resourcesDir = "${basedir}/resources"
+final descriptorsDir = "${basedir}/descriptors"
+
+target(cleanResources: "Removes the existing jbilling resources directory.") {
+    delete(dir: "${resourcesDir}")
+}
+
+target(createStructure: "Creates the jbilling resources directory structure.") {
+    ant.sequential {
+        mkdir(dir: "${resourcesDir}")
+        mkdir(dir: "${resourcesDir}/designs")
+        mkdir(dir: "${resourcesDir}/invoices")
+        mkdir(dir: "${resourcesDir}/logos")
+        mkdir(dir: "${resourcesDir}/mediation")
+        mkdir(dir: "${resourcesDir}/mediation/errors")
+        mkdir(dir: "${resourcesDir}/reports")
+    }
+}
+
+target(main: "Creates the jbilling 'resources/' directories and copies necessary files.") {
+    depends(cleanResources, createStructure)
+
+    // copy default company logos
+    copy(todir: "${resourcesDir}/logos") {
+        fileset(dir: "${descriptorsDir}/logos")
+    }
+
+    // copy default mediation files
+    copy(todir: "${resourcesDir}/mediation") {
+        fileset(dir: "${descriptorsDir}/mediation", includes: "mediation.dtd")
+        fileset(dir: "${descriptorsDir}/mediation", includes: "asterisk.xml")
+    }
+}
+
+setDefaultTarget(main)

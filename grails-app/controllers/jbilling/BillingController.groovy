383f313ff88343d8c7c1e2e57bd3b6a5c2f2672c
vikasbo <vikasb@jbilling.com>
2011-10-03 23:19:04 +0530

    Fixed: Added error message when billing proces id does not exist
    Conflicts:
    
    	grails-app/controllers/jbilling/BillingController.groovy

--- grails-app/controllers/jbilling/BillingController.groovy
+++ grails-app/controllers/jbilling/BillingController.groovy
@@ -117,10 +117,17 @@ class BillingController {
 	 * To display the run details of a given Process Id
 	 */
 	def show = {
-		Integer processId = params.int('id')
-		BillingProcessDTO process = new BillingProcessDAS().find(processId);
 
-        def das = new BillingProcessDAS()
+		def processId = params.int('id')
+        
+        if ( !BillingProcessDTO.exists( processId ) ) {
+            flash.error = 'billing.process.review.doesnotexist'
+            flash.args = [processId]
+            redirect action:'list'
+        }
+        
+        BillingProcessDTO process = BillingProcessDTO.get(processId);
+        
 		def genInvoices = new InvoiceDAS().findByProcess(process)
 		def invoicesGenerated = genInvoices?.size() ?: 0
 

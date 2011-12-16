e56812bb249cafda0ad05592ded2e80d17628da5
Brian Cowdery <brianc@jbilling.com>
2011-12-16 10:23:12 -0800

    CC startup script run grails from GRAILS_HOME

--- startup.sh
+++ startup.sh
@@ -6,7 +6,6 @@
 #
 # see also 'cc-build.properties'
 
-
 # load properties if file exists, otherwise use port 8080
 if [ -f cc-build.properties ]; then
     . cc-build.properties
@@ -18,7 +17,7 @@ fi
 export GRAILS_OPTS="-server -Xmx1024M -Xms256M -XX:MaxPermSize=256m"
 
 # start jbilling and record process id
-nohup grails -Ddisable.auto.recompile=true -Dserver.port=${server_port} run-app &
+nohup $GRAILS_HOME/bin/grails -Ddisable.auto.recompile=true -Dserver.port=${server_port} run-app &
 echo $!> jbilling.pid
 
 echo "Started jBilling on port ${server_port}."

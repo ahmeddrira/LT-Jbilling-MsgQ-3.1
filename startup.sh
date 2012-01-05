3e53c4e46ce7b07f701994308ecce0d6c0b02f36
Brian Cowdery <brianc@jbilling.com>
2012-01-05 13:10:14 -0700

    License header in scripts

--- startup.sh
+++ startup.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 #
+# JBILLING CONFIDENTIAL
+# _____________________
+#
+# [2003] - [2012] Enterprise jBilling Software Ltd.
+# All Rights Reserved.
+#
+# NOTICE:  All information contained herein is, and remains
+# the property of Enterprise jBilling Software.
+# The intellectual and technical concepts contained
+# herein are proprietary to Enterprise jBilling Software
+# and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden.
+
+
 # Startup script for jBilling under Cruise Control. This script sets the
 # server port on the running jBilling instance so that multiple jBilling 
 # projects can be tested under the same Cruise Control build loop.
 #
 # see also 'cc-build.properties'
+
 
 # load properties if file exists, otherwise use port 8080
 if [ -f cc-build.properties ]; then

16865d71e32bdce89eb24415eff1edbdaa2a4eb5
Vikas Bodani <vikasb@jbilling.com>
2012-05-01 17:18:59 +0530

    Upgraded documentation * Using 2.0 version of documentation * Custom style sheets

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -112,6 +112,7 @@ grails.doc.license="AGPL v3"
 grails.doc.images=new File("src/docs/images")
 grails.doc.api.org.springframework="http://static.springsource.org/spring/docs/3.0.x/javadoc-api/"
 grails.doc.api.org.hibernate="http://docs.jboss.org/hibernate/stable/core/javadocs/"
+grails.doc.api.java="http://docs.oracle.com/javase/6/docs/api/"
 
 //gdoc aliases
 grails.doc.alias.userGuide="1. jBilling User Guide"

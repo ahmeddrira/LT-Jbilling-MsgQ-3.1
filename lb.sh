ec549cc90375b7d3f91fad24652b3abb313a5ed0
Emiliano Conde <emilc@jbilling.com>
2012-05-21 16:47:17 -0400

    To avoid everybody having to install Liquibase, better put it in the repo

--- lb.sh
+++ lb.sh
@@ -1,4 +1,4 @@
-liquibase --driver=org.postgresql.Driver \
+liquidbase-2.0.5/liquibase --driver=org.postgresql.Driver \
       --classpath=lib/postgresql-8.4-702.jdbc4.jar \
       --changeLogFile=descriptors/database/jbilling-schema.xml \
       --url="jdbc:postgresql://localhost:5432/jbilling_test" \

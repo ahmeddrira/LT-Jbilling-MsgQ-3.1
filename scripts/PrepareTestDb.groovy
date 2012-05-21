abb8d1d323e6c6cf772b942fe2e17509f6e73f80
Emiliano Conde <emilc@jbilling.com>
2012-05-21 17:17:44 -0400

    The 'grails prepare-test-db' script now uses Liquibase instead of 'jbilling_test.sql'

--- scripts/PrepareTestDb.groovy
+++ scripts/PrepareTestDb.groovy
@@ -18,37 +18,39 @@ includeTargets << grailsScript("Init")
 
 target(prepareTestDb: "Import the test postgresql database.") {
 
-    // load the client reference DB if it exists
-    def testDb = new File("${basedir}/sql/jbilling_test.sql")
-    def referenceDb = new File("${basedir}/data.sql")
-    def file = referenceDb.exists() ? referenceDb : testDb
-
     // optionally accept database name and user name arguments
     parseArguments();
-    def cleanDb = argsMap.cleanDb;
     def username = argsMap.user ? argsMap.user : "jbilling"
     def database = argsMap.db ? argsMap.db : "jbilling_test"
 
-    if (cleanDb) {
-        println "Dropping a database: ${database}..."
-        // call postgresl to drop database
-        exec(executable: "dropdb", failonerror: false) {
-            arg(line: "-U ${username} -e ${database}")
-        }
-        println "Done."
+    println "Dropping a database: ${database}..."
+    // call postgresl to drop database
+    exec(executable: "dropdb", failonerror: false) {
+        arg(line: "-U ${username} -e ${database}")
+    }
+    println "Done."
 
-        println "Creating a database: ${database}..."
-        // call postgresl to create database
-        exec(executable: "createdb", failonerror: true) {
-            arg(line: "-U ${username} -O ${username} -E UTF-8 -e ${database}")
-        }
-        println "Done."
+    println "Creating a database: ${database}..."
+    // call postgresl to create database
+    exec(executable: "createdb", failonerror: true) {
+        arg(line: "-U ${username} -O ${username} -E UTF-8 -e ${database}")
+    }
+    println "Done."
+
+    println "Importing file test database into the ${database} database (user: ${username})"
+    // call liquibase to load the database base schema
+    exec(executable: "./lb.sh", failonerror: false) {
+        arg(line: "--contexts=base update")
     }
 
-    println "Importing file '${file.name}' into the ${database} database (user: ${username})"
-    // call postgresl to load the database
-    exec(executable: "psql", failonerror: false) {
-        arg(line: "-U ${username} -f ${file.path} ${database}")
+    // call liquibase to load the database data
+    exec(executable: "liquidbase-2.0.5/liquibase", failonerror: false) {
+        arg(line: "--driver=org.postgresql.Driver --classpath=lib/postgresql-8.4-702.jdbc4.jar --changeLogFile=descriptors/database/jbilling-test_data.xml --url=\"jdbc:postgresql://localhost:5432/jbilling_test\" --username=jbilling --password= update")
+    }
+
+    // call liquibase to load the database foreign keys
+    exec(executable: "./lb.sh", failonerror: false) {
+        arg(line: "--contexts=FKs update")
     }
 
     println "Done."
